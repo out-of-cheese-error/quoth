f06c37ffc2705aa1a2b47f78f6d1695c4bc46057
Ninjani <48680156+Ninjani@users.noreply.github.com>
2020-04-22 13:36:16 +0200

    removed thiserror example

--- src/errors.rs
+++ src/errors.rs
@@ -1,21 +1,5 @@
 use thiserror::Error;
 
-//
-//#[derive(Error, Debug)]
-//pub enum DataStoreError {
-//    #[error("data store disconnected")]
-//    Disconnect(#[source] io::Error),
-//    #[error("the data for key `{0}` is not available")]
-//    Redaction(String),
-//    #[error("invalid header (expected {expected:?}, found {found:?})")]
-//    InvalidHeader {
-//        expected: String,
-//        found: String,
-//    },
-//    #[error("unknown data store error")]
-//    Unknown,
-//}
-
 /// Errors which can be caused by normal quoth operation.
 /// Those caused by external libraries throw their own errors
 #[derive(Debug, Error)]
